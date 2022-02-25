99d51e0d1e5ab749e3c719a68f4df37932a4533b
Gautam Bose <gautam.bos3@gmail.com>
2022-02-25 12:52:50 -0800

    adds maps lib

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import App from './App.svelte';
-
+const MAPS_API_KEY = 'AIzaSyAgBe8TaYCDQBr-xxyh0x1otjwUpeGuc4k'
+window.initMap = function ready() {
+	app.$set({ ready: true });
+}
 const app = new App({
 	target: document.body,
 	props: {
-		name: 'world'
+		ready: false,
 	}
 });
 
